f34e487376cbd82b8d3e5009e610c3b5523d5db3
bakhytgulAkhmetova1 <omron2020@bk.ru>
2020-07-30 00:29:00 +0400

    task2 completed

--- module_1/task2.js
+++ module_1/task2.js
@@ -1,15 +1,49 @@
 const fs = require('fs');
 const csv = require('csvtojson');
+const zlib = require('zlib');
+const { pipeline } = require('stream');
+const readline = require('readline');
 
-csv({ colParser:{
-    ''
-} })
-    .fromFile('./csv/cities.csv')
+const CSV_PATH = './csv/books.csv';
+const csvOptions = {
+    noheader: false,
+    headers: ['book', 'author', 'amount', 'price'],
+    colParser: {
+        'amount': function (item) { return +item },
+        'price': function (item) { return +item }
+    }
+};
+
+//Load all content into RAM
+
+csv(csvOptions).fromFile(CSV_PATH)
     .then((csvObject) => {
-        console.log('[csvObject]', csvObject);
-        fs.writeFile('cities.txt', csvObject, (error) => {
+        const cities = formattedData(csvObject);
+        fs.writeFile('cities.txt', cities, (error) => {
             if (error) {
-                console.log('[error]', error);
+                //handle error
             }
         })
     });
+
+
+const formattedData = (data) => {
+    if (Array.isArray(data)) {
+        return data.map(rowObj => (JSON.stringify(rowObj))).join('\r\n');
+    }
+}
+
+//Do not load all content into RAM
+
+function processFile() {
+    const readStream = fs.createReadStream(CSV_PATH);
+    const writeStream = fs.createWriteStream('file2.txt');
+    const lineReader = readline.createInterface({ input: csv(csvOptions).fromStream(readStream) });
+
+    lineReader.on('line', function (line) {
+        writeStream.write(line + '\r\n')
+    });
+}
+
+processFile();
+
